9f413167a69aea2e6d66d34ed917ea5a1b065aae
digetl <dave@digetl.co.uk>
2020-07-06 16:38:39 +0100

    Created movies compenent

---
+++ src/components/Movies.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {
+    render() {
+        return(
+            <div>Good films to see this year!</div>
+        )
+    }
+}
+
+export default Movies;
